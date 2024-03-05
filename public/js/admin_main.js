cd66f6ad0434ccb1c1a2fee0a9e0b8882abde098
baotoan810 <you@example.com>
2024-03-05 22:37:28 +0700

    update code

--- public/js/admin_main.js
+++ public/js/admin_main.js
@@ -1,47 +1,99 @@
 // nav
 $(document).ready(function () {
-    $("#item_cha").click(function (e) { 
+    $("#item_cha").click(function (e) {
         $("#item-con").fadeIn(500).css("display", "flex"); // 500 milliseconds (0.5s) là thời gian của hiệu ứng
     });
-    $("#close_nav").click(function (e) { 
+    $("#close_nav").click(function (e) {
         $("#item-con").fadeIn(500).css("display", "none"); // 500 milliseconds (0.5s) là thời gian của hiệu ứng
     });
-    
-    // $(".item_cha").mouseenter(function () {
-    //     $(".item-con").fadeIn(500).css("display", "flex");
-    // });
-
 
 });
+
+//================= todo: Item 1 ===========================================
 $(document).ready(function () {
-    $("#item_cha").click(function (e) { 
-        $("#item_cha").fadeIn(500).css("display", "none"); // 500 milliseconds (0.5s) là thời gian của hiệu ứng
-    });
-});
-
-
-
-
-$(document).ready(function () {
-    $("#item_cha1").click(function (e) { 
+    $("#item_cha1").click(function (e) {
         $("#item-con1").fadeIn(500).css("display", "flex"); // 500 milliseconds (0.5s) là thời gian của hiệu ứng
     });
 
-    $("#close_nav1").click(function (e) { 
+
+});
+$(document).ready(function () {
+    $("#close_nav1").click(function (e) {
         $("#item-con1").fadeIn(500).css("display", "none"); // 500 milliseconds (0.5s) là thời gian của hiệu ứng
     });
 
-    
-    // $(".item_cha").mouseenter(function () {
-    //     $(".item-con").fadeIn(500).css("display", "flex");
-    // });
+
+});
+
+//================= todo: Item 2 ===========================================
+
+$(document).ready(function () {
+    $("#item_cha2").click(function (e) {
+        $("#item-con2").fadeIn(500).css("display", "flex"); // 500 milliseconds (0.5s) là thời gian của hiệu ứng
+    });
 
 
 });
 $(document).ready(function () {
-    $("#item_cha1").click(function (e) { 
-        $("#item_cha1").fadeIn(500).css("display", "none"); // 500 milliseconds (0.5s) là thời gian của hiệu ứng
+    $("#close_nav2").click(function (e) {
+        $("#item-con2").fadeIn(500).css("display", "none"); // 500 milliseconds (0.5s) là thời gian của hiệu ứng
     });
-    
+
 
 });
+
+//================= todo: Item 3 ===========================================
+
+$(document).ready(function () {
+    $("#item_cha3").click(function (e) {
+        $("#item-con3").fadeIn(500).css("display", "flex"); // 500 milliseconds (0.5s) là thời gian của hiệu ứng
+    });
+
+
+});
+$(document).ready(function () {
+    $("#close_nav3").click(function (e) {
+        $("#item-con3").fadeIn(500).css("display", "none"); // 500 milliseconds (0.5s) là thời gian của hiệu ứng
+    });
+
+
+});
+
+
+
+//================= todo: Item 4 ===========================================
+
+$(document).ready(function () {
+    $("#item_cha4").click(function (e) {
+        $("#item-con4").fadeIn(500).css("display", "flex"); // 500 milliseconds (0.5s) là thời gian của hiệu ứng
+    });
+
+
+});
+$(document).ready(function () {
+    $("#close_nav4").click(function (e) {
+        $("#item-con4").fadeIn(500).css("display", "none"); // 500 milliseconds (0.5s) là thời gian của hiệu ứng
+    });
+
+
+});
+
+//================= todo: Item 5 ===========================================
+
+$(document).ready(function () {
+    $("#item_cha5").click(function (e) {
+        $("#item-con5").fadeIn(500).css("display", "flex"); // 500 milliseconds (0.5s) là thời gian của hiệu ứng
+    });
+
+
+});
+$(document).ready(function () {
+    $("#close_nav5").click(function (e) {
+        $("#item-con5").fadeIn(500).css("display", "none"); // 500 milliseconds (0.5s) là thời gian của hiệu ứng
+    });
+
+
+});
+
+
+
