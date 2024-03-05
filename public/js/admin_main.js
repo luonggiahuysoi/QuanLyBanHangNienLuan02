62794312c7793e593ff91e9971ac4b7cdf2282ba
luonggiahuysoiluonggiahuysoi <luonggiahuy090203@gmail.com>
2024-03-05 21:58:07 +0700

    huyne

---
+++ public/js/admin_main.js
@@ -0,0 +1,47 @@
+// nav
+$(document).ready(function () {
+    $("#item_cha").click(function (e) { 
+        $("#item-con").fadeIn(500).css("display", "flex"); // 500 milliseconds (0.5s) là thời gian của hiệu ứng
+    });
+    $("#close_nav").click(function (e) { 
+        $("#item-con").fadeIn(500).css("display", "none"); // 500 milliseconds (0.5s) là thời gian của hiệu ứng
+    });
+    
+    // $(".item_cha").mouseenter(function () {
+    //     $(".item-con").fadeIn(500).css("display", "flex");
+    // });
+
+
+});
+$(document).ready(function () {
+    $("#item_cha").click(function (e) { 
+        $("#item_cha").fadeIn(500).css("display", "none"); // 500 milliseconds (0.5s) là thời gian của hiệu ứng
+    });
+});
+
+
+
+
+$(document).ready(function () {
+    $("#item_cha1").click(function (e) { 
+        $("#item-con1").fadeIn(500).css("display", "flex"); // 500 milliseconds (0.5s) là thời gian của hiệu ứng
+    });
+
+    $("#close_nav1").click(function (e) { 
+        $("#item-con1").fadeIn(500).css("display", "none"); // 500 milliseconds (0.5s) là thời gian của hiệu ứng
+    });
+
+    
+    // $(".item_cha").mouseenter(function () {
+    //     $(".item-con").fadeIn(500).css("display", "flex");
+    // });
+
+
+});
+$(document).ready(function () {
+    $("#item_cha1").click(function (e) { 
+        $("#item_cha1").fadeIn(500).css("display", "none"); // 500 milliseconds (0.5s) là thời gian của hiệu ứng
+    });
+    
+
+});
