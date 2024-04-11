b5ae118fe1d1dea53f55e7db6d347b91b8b45a77
baotoan810 <you@example.com>
2024-04-11 19:53:49 +0700

    up code

--- user/view/trangchu/footer.php
+++ user/view/trangchu/footer.php
@@ -1,59 +1,59 @@
-   <!--================- END: MAIN  ===================-->
+<!--================- END: MAIN  ===================-->
 
 
-   <footer>
-       <div class="footer-main">
-           <div class="footer__main-infor">
-               <img src="../public/img/logo.png" alt="">
-               <p class="footer-address"><i class="bi bi-geo-alt"></i>Địa chỉ: số 32a Hưng Phú, Cái Răng, CT</p>
-               <p class="footer-hotline"><i class="bi bi-telephone"></i>Hotline: 09837637</p>
-               <p class="footer-email"><i class="bi bi-envelope"></i>Email: Iloveshop@gmail.com</p>
-               <div class="footer-network">
-                   <img src="../public/img/facebook.png" alt="">
-                   <img src="../public/img/youtobe.png" alt="">
-                   <img src="../public/img/instagram.jpg" alt="">
-               </div>
-           </div>
+<footer>
+    <div class="footer-main">
+        <div class="footer__main-infor">
+            <img src="../public/img/logo.png" alt="">
+            <p class="footer-address"><i class="bi bi-geo-alt"></i>Địa chỉ: số 32a Hưng Phú, Cái Răng, CT</p>
+            <p class="footer-hotline"><i class="bi bi-telephone"></i>Hotline: 09837637</p>
+            <p class="footer-email"><i class="bi bi-envelope"></i>Email: Iloveshop@gmail.com</p>
+            <div class="footer-network">
+                <img src="../public/img/facebook.png" alt="">
+                <img src="../public/img/youtobe.png" alt="">
+                <img src="../public/img/instagram.jpg" alt="">
+            </div>
+        </div>
 
-           <div class="footer__main-help">
-               <h3>Hỗ trợ khách hàng</h3>
-               <ul class="footer__main-help--list">
-                   <li>Hệ thống cửa hàng</li>
-                   <li>Câu hỏi thường gặp</li>
-                   <li>Kiểm tra hàng</li>
-                   <li>Liên hệ</li>
-               </ul>
-           </div>
+        <div class="footer__main-help">
+            <h3>Hỗ trợ khách hàng</h3>
+            <ul class="footer__main-help--list">
+                <li>Hệ thống cửa hàng</li>
+                <li>Câu hỏi thường gặp</li>
+                <li>Kiểm tra hàng</li>
+                <li>Liên hệ</li>
+            </ul>
+        </div>
 
-           <div class="footer__main-policy">
-               <h3>Chính sách hỗ trợ</h3>
-               <ul class="footer__main-help--list">
-                   <li>Chính sách bảo hành</li>
-                   <li>Chính sách đổi trả</li>
-                   <li>Chính sách bảo mật</li>
-               </ul>
-           </div>
+        <div class="footer__main-policy">
+            <h3>Chính sách hỗ trợ</h3>
+            <ul class="footer__main-help--list">
+                <li>Chính sách bảo hành</li>
+                <li>Chính sách đổi trả</li>
+                <li>Chính sách bảo mật</li>
+            </ul>
+        </div>
 
-           <div class="footer__main-sale">
-               <h2>Đăng ký nhận ưu đãi</h2>
-               <p>Bạn muốn nhận khuyến mãi đặc biệt? Đăng kí tham gia ngay cộng động hơn 68.000+ người theo dõi của
-                   chúng tôi để cập nhật khuyến mãi ngay lập tức</p>
+        <div class="footer__main-sale">
+            <h2>Đăng ký nhận ưu đãi</h2>
+            <p>Bạn muốn nhận khuyến mãi đặc biệt? Đăng kí tham gia ngay cộng động hơn 68.000+ người theo dõi của
+                chúng tôi để cập nhật khuyến mãi ngay lập tức</p>
 
-               <div class="footer__main-sale--email">
-                   <input type="email" placeholder="Nhập email nhận ưu đãi">
-                   <p>Đăng ký</p>
-               </div>
-           </div>
-       </div>
+            <div class="footer__main-sale--email">
+                <input type="email" placeholder="Nhập email nhận ưu đãi">
+                <p>Đăng ký</p>
+            </div>
+        </div>
+    </div>
 
-       <div class="footer-end">
-           <p>© Bản quyền thuộc về EGANY | Cung cấp bởi Sapo</p>
-       </div>
+    <div class="footer-end">
+        <p>© Bản quyền thuộc về EGANY | Cung cấp bởi Sapo</p>
+    </div>
 
-   </footer>
+</footer>
 
-   <!-- js  -->
-   <script src="../public/js/script.js"></script>
-   </body>
+<!-- js  -->
+<script src="../public/js/script.js"></script>
+</body>
 
-   </html>
+</html>
