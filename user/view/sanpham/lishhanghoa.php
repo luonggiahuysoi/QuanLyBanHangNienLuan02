c86055e623d1eb9132b6dce64356a132828be78d
luonggiahuysoiluonggiahuysoi <luonggiahuy090203@gmail.com>
2024-04-11 20:33:11 +0700

    css lai

--- user/view/sanpham/lishhanghoa.php
+++ user/view/sanpham/lishhanghoa.php
@@ -14,305 +14,85 @@
 
         <div class="product__full">
             <div class="main__suggest-product">
-                <div class="main__product-item">
-                    <img src="./public/img/giay.jpg" alt="" onmouseover="changeImage(this)" onmouseout="restoreImage(this)">
-                    <p class="main__product-item--name">Giày thể thao nam</p>
-                    <div class="main__product-item-price">
-                        <p>1.999.999đ</p>
-                        <p>2.500.000đ</p>
-                    </div>
-                    <div class="main__product-item-color">
-                        <p style="background-color: black;     box-shadow: 0 0 0 2px black, inset 0 0 0 2px rgb(230, 224, 224);
-                "></p>
-                        <p style="background-color: rgb(41, 147, 240); box-shadow: 0 0 0 2px rgb(41, 147, 240), inset 0 0 0 2px rgb(230, 224, 224)">
-                        </p>
-                    </div>
-                    <div class="main__product-item-quanity">
-                        <p>
-                            Số lương còn lại:
-                        </p>
-                        <p>
-                            x2 Sản phẩm
-                        </p>
-                    </div>
 
-                    <div class="main__product-item--sale">
-                        <p>10%</p>
-                    </div>
+            <?php
+                    $tinhgiasale = 0;
+                    foreach ($lishHangHoa as $lishHangHoaa) {
+                        extract($lishHangHoaa);
+                        $tinhgiasale = $gia * (1 - $giasale / 100);
+                        if($lishHangHoaa['soluong']>0) {
+                            echo '<div class="main__product-item">';
+                                echo '<a href = "">';
+                                    echo '<img src="data:image/png;base64,' . $hinhanh . '" alt="">';
+                                    echo '<p class="main__product-item--name">'.$tenhanghoa.'</p>';
+                                    echo '<div class="main__product-item-price">';
+                                    echo '<p>' . number_format($tinhgiasale) . ' <span>vnđ</span></p>';
+                                    echo '<p>' . number_format($gia) . ' <span>vnđ</span></p>';
+                                    echo '</div>';
+            
+                                    // Hiển thị màu cho sản phẩm
+                                    echo '<div class="main__product-item-color">';
+                                    $mau = new DanhMuc;
+                                    $showmau = $mau->ShowMau($id_hanghoa);
+                                    foreach ($showmau as $mauu) {
+                                        echo '<p style="background-color: ' . $mauu['mausac'] . ';"></p>';
+                                    }
+                                    echo '</div>';
+            
+                                    echo '<div class="main__product-item-quanity">';
+                                    echo '<p>Số lương còn lại:</p>';
+                                    echo '<p>x' . $soluong . '</p>';
+                                    echo '</div>';
+                                    echo '<div class="main__product-item--sale">';
+                                    echo '<p>' . $giasale . '%</p>';
+                                    echo '</div>';
+                                    echo '<div class="main__children-product--item">';
+                                    echo '<a href="">Thêm vào giỏ</a>';
+                                    echo '</div>';
+                                echo '</a>';
+                            echo '</div>';
+                        } else {
 
-                    <div class="main__product-item--sale">
-                        <p>10%</p>
-                    </div>
+                            echo '<div class="main__product-item main__suggest-opacity">';
+                            echo '<img src="data:image/png;base64,' . $hinhanh . '" alt="">';
+                            echo '<p class="main__product-item--name">Giày thể thao nam</p>';
+                            echo '<div class="main__product-item-price">';
+                            echo '<p>' . number_format($tinhgiasale) . ' <span>vnđ</span></p>';
+                            echo '<p>' . number_format($gia) . ' <span>vnđ</span></p>';
+                            echo '</div>';
+    
+                            // Hiển thị màu cho sản phẩm
+                            echo '<div class="main__product-item-color">';
+                            $mau = new DanhMuc;
+                            $showmau = $mau->ShowMau($id_hanghoa);
+                            foreach ($showmau as $mauu) {
+                                echo '<p style="background-color: ' . $mauu['mausac'] . ';"></p>';
+                            }
+                            echo '</div>';
+    
+                            echo '<div class="main__product-item-quanity">';
+                            echo '<p>Số lương còn lại:</p>';
+                            echo '<p>x' . $soluong . '</p>';
+                            echo '</div>';
+                            echo '<div class="main__product-item--sale">';
+                            echo '<p>' . $giasale . '%</p>';
+                            echo '</div>';
+                            echo '<div class="main__children-product--item">';
+                            echo '<a href="">Thêm vào giỏ</a>';
+                            echo '</div>';
+                            echo '<div class="main__product-sale--end">
+                                        <p>Hết hàng</p>
+                                    </div>';
+                            echo '</div>';
+                            
+                        }
+                    }
 
-                    <div class="main__children-product--item">
-                        <a href="">Thêm vào giỏ</a>
-                    </div>
-                </div>
-
-                <div class="main__product-item main__suggest-opacity">
-                    <img src="./public/img/balo.jpg" alt="">
-                    <p class="main__product-item--name">Giày thể thao nam</p>
-                    <div class="main__product-item-price">
-                        <p>1.999.999đ</p>
-                        <p>2.500.000đ</p>
-                    </div>
-                    <div class="main__product-item-color">
-                        <p style="background-color: black;     box-shadow: 0 0 0 2px black, inset 0 0 0 2px rgb(230, 224, 224);
-                "></p>
-                        <p style="background-color: rgb(41, 147, 240); box-shadow: 0 0 0 2px rgb(41, 147, 240), inset 0 0 0 2px rgb(230, 224, 224)">
-                        </p>
-                    </div>
-                    <div class="main__product-item-quanity">
-                        <p>
-                            Số lương còn lại:
-                        </p>
-                        <p>
-                            x2 Sản phẩm
-                        </p>
-                    </div>
-
-                    <div class="main__product-item--sale">
-                        <p>10%</p>
-                    </div>
-
-                    <div class="main__product-item--sale">
-                        <p>10%</p>
-                    </div>
-
-                    <div class="main__children-product--item">
-                        <a href="">Thêm vào giỏ</a>
-                    </div>
-
-                    <div class="main__product-sale--end">
-                        <p>Hết hàng</p>
-                    </div>
-
-
-
-
-
-                </div>
-
-                <div class="main__product-item">
-                    <img src="./public/img/non1.jpg" alt="">
-                    <p class="main__product-item--name">Giày thể thao nam</p>
-                    <div class="main__product-item-price">
-                        <p>1.999.999đ</p>
-                        <p>2.500.000đ</p>
-                    </div>
-                    <div class="main__product-item-color">
-                        <p style="background-color: black;     box-shadow: 0 0 0 2px black, inset 0 0 0 2px rgb(230, 224, 224);
-                "></p>
-                        <p style="background-color: rgb(41, 147, 240); box-shadow: 0 0 0 2px rgb(41, 147, 240), inset 0 0 0 2px rgb(230, 224, 224)">
-                        </p>
-                    </div>
-                    <div class="main__product-item-quanity">
-                        <p>
-                            Số lương còn lại:
-                        </p>
-                        <p>
-                            x2 Sản phẩm
-                        </p>
-                    </div>
-
-                    <div class="main__product-item--sale">
-                        <p>10%</p>
-                    </div>
-
-                    <div class="main__product-item--sale">
-                        <p>10%</p>
-                    </div>
-
-                    <div class="main__children-product--item">
-                        <a href="">Thêm vào giỏ</a>
-                    </div>
-                </div>
-
-                <div class="main__product-item">
-                    <img src="./public/img/quansorrt.jpg" alt="">
-                    <p class="main__product-item--name">Giày thể thao nam</p>
-                    <div class="main__product-item-price">
-                        <p>1.999.999đ</p>
-                        <p>2.500.000đ</p>
-                    </div>
-                    <div class="main__product-item-color">
-                        <p style="background-color: black;     box-shadow: 0 0 0 2px black, inset 0 0 0 2px rgb(230, 224, 224);
-                "></p>
-                        <p style="background-color: rgb(41, 147, 240); box-shadow: 0 0 0 2px rgb(41, 147, 240), inset 0 0 0 2px rgb(230, 224, 224)">
-                        </p>
-                    </div>
-                    <div class="main__product-item-quanity">
-                        <p>
-                            Số lương còn lại:
-                        </p>
-                        <p>
-                            x2 Sản phẩm
-                        </p>
-                    </div>
-
-                    <div class="main__product-item--sale">
-                        <p>10%</p>
-                    </div>
-
-                    <div class="main__product-item--sale">
-                        <p>10%</p>
-                    </div>
-                    <div class="main__children-product--item">
-                        <a href="">Thêm vào giỏ</a>
-                    </div>
-                </div>
-            </div>
-            <div class="main__suggest-product">
-                <div class="main__product-item">
-                    <img src="./public/img/giay.jpg" alt="" onmouseover="changeImage(this)" onmouseout="restoreImage(this)">
-                    <p class="main__product-item--name">Giày thể thao nam</p>
-                    <div class="main__product-item-price">
-                        <p>1.999.999đ</p>
-                        <p>2.500.000đ</p>
-                    </div>
-                    <div class="main__product-item-color">
-                        <p style="background-color: black;     box-shadow: 0 0 0 2px black, inset 0 0 0 2px rgb(230, 224, 224);
-                "></p>
-                        <p style="background-color: rgb(41, 147, 240); box-shadow: 0 0 0 2px rgb(41, 147, 240), inset 0 0 0 2px rgb(230, 224, 224)">
-                        </p>
-                    </div>
-                    <div class="main__product-item-quanity">
-                        <p>
-                            Số lương còn lại:
-                        </p>
-                        <p>
-                            x2 Sản phẩm
-                        </p>
-                    </div>
-
-                    <div class="main__product-item--sale">
-                        <p>10%</p>
-                    </div>
-
-                    <div class="main__product-item--sale">
-                        <p>10%</p>
-                    </div>
-
-                    <div class="main__children-product--item">
-                        <a href="">Thêm vào giỏ</a>
-                    </div>
-                </div>
-
-                <div class="main__product-item main__suggest-opacity">
-                    <img src="./public/img/balo.jpg" alt="">
-                    <p class="main__product-item--name">Giày thể thao nam</p>
-                    <div class="main__product-item-price">
-                        <p>1.999.999đ</p>
-                        <p>2.500.000đ</p>
-                    </div>
-                    <div class="main__product-item-color">
-                        <p style="background-color: black;     box-shadow: 0 0 0 2px black, inset 0 0 0 2px rgb(230, 224, 224);
-                "></p>
-                        <p style="background-color: rgb(41, 147, 240); box-shadow: 0 0 0 2px rgb(41, 147, 240), inset 0 0 0 2px rgb(230, 224, 224)">
-                        </p>
-                    </div>
-                    <div class="main__product-item-quanity">
-                        <p>
-                            Số lương còn lại:
-                        </p>
-                        <p>
-                            x2 Sản phẩm
-                        </p>
-                    </div>
-
-                    <div class="main__product-item--sale">
-                        <p>10%</p>
-                    </div>
-
-                    <div class="main__product-item--sale">
-                        <p>10%</p>
-                    </div>
-
-                    <div class="main__children-product--item">
-                        <a href="">Thêm vào giỏ</a>
-                    </div>
-
-                    <div class="main__product-sale--end">
-                        <p>Hết hàng</p>
-                    </div>
+                    ?>
 
 
-
-
-
-                </div>
-
-                <div class="main__product-item">
-                    <img src="./public/img/non1.jpg" alt="">
-                    <p class="main__product-item--name">Giày thể thao nam</p>
-                    <div class="main__product-item-price">
-                        <p>1.999.999đ</p>
-                        <p>2.500.000đ</p>
-                    </div>
-                    <div class="main__product-item-color">
-                        <p style="background-color: black;     box-shadow: 0 0 0 2px black, inset 0 0 0 2px rgb(230, 224, 224);
-                "></p>
-                        <p style="background-color: rgb(41, 147, 240); box-shadow: 0 0 0 2px rgb(41, 147, 240), inset 0 0 0 2px rgb(230, 224, 224)">
-                        </p>
-                    </div>
-                    <div class="main__product-item-quanity">
-                        <p>
-                            Số lương còn lại:
-                        </p>
-                        <p>
-                            x2 Sản phẩm
-                        </p>
-                    </div>
-
-                    <div class="main__product-item--sale">
-                        <p>10%</p>
-                    </div>
-
-                    <div class="main__product-item--sale">
-                        <p>10%</p>
-                    </div>
-
-                    <div class="main__children-product--item">
-                        <a href="">Thêm vào giỏ</a>
-                    </div>
-                </div>
-
-                <div class="main__product-item">
-                    <img src="./public/img/quansorrt.jpg" alt="">
-                    <p class="main__product-item--name">Giày thể thao nam</p>
-                    <div class="main__product-item-price">
-                        <p>1.999.999đ</p>
-                        <p>2.500.000đ</p>
-                    </div>
-                    <div class="main__product-item-color">
-                        <p style="background-color: black;     box-shadow: 0 0 0 2px black, inset 0 0 0 2px rgb(230, 224, 224);
-                "></p>
-                        <p style="background-color: rgb(41, 147, 240); box-shadow: 0 0 0 2px rgb(41, 147, 240), inset 0 0 0 2px rgb(230, 224, 224)">
-                        </p>
-                    </div>
-                    <div class="main__product-item-quanity">
-                        <p>
-                            Số lương còn lại:
-                        </p>
-                        <p>
-                            x2 Sản phẩm
-                        </p>
-                    </div>
-
-                    <div class="main__product-item--sale">
-                        <p>10%</p>
-                    </div>
-
-                    <div class="main__product-item--sale">
-                        <p>10%</p>
-                    </div>
-                    <div class="main__children-product--item">
-                        <a href="">Thêm vào giỏ</a>
-                    </div>
-                </div>
             </div>
         </div>
-
 
     </div>
 
@@ -381,51 +161,48 @@
 </html>
 
 <style>
-   .main-product {
-    margin-top: 10%;
-}
+    .main-product {
+        margin-top: 10%;
+    }
 
-.main__product-btn {
-    display: flex;
-    justify-content: center;
-    align-items: center;
-    gap: 40px;
-}
+    .main__product-btn {
+        display: flex;
+        justify-content: center;
+        align-items: center;
+        gap: 40px;
+    }
 
-.main__product-btn input {
-    border-radius: 10px;
-    padding: 6px 12px;
-    background-color: #ffffff;
-    border: 1px solid #444;
-    font-weight: 500;
-    font-family: Arial, Helvetica, sans-serif;
-    cursor: pointer;
-}
+    .main__product-btn input {
+        border-radius: 10px;
+        padding: 6px 12px;
+        background-color: #ffffff;
+        border: 1px solid #444;
+        font-weight: 500;
+        font-family: Arial, Helvetica, sans-serif;
+        cursor: pointer;
+    }
 
-.main__product-btn input:hover {
-    color: #186F65;
-    font-weight: 550;
-}
+    .main__product-btn input:hover {
+        color: #186F65;
+        font-weight: 550;
+    }
 
-.product__full {
-    width: 100%;
-    height: 100%;
-    background-color: #4444;
-    margin-top: 2%;
-    padding: 40px 0;
+    .product__full {
+        width: 100%;
+        height: 100%;
+        background-color: #4444;
+        margin-top: 2%;
+        padding: 40px 0;
 
-    background: linear-gradient(to right, #99f2c8, #1f4037);
-
-
-}
-
-.main__suggest {
-
-}
+        background: linear-gradient(to right, #99f2c8, #1f4037);
 
 
-footer {
-    margin-top: -30%;
-}
+    }
 
+    .main__suggest {}
+
+
+    footer {
+        margin-top: -30%;
+    }
 </style>
